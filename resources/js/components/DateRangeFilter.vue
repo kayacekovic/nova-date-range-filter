edd5b95f868079ec851e171f904059d5e5790c6b
Timurhan Kaya <kayacekovic@gmail.com>
2021-12-27 21:39:42 +0300

    add clear btn and turkish localize

--- resources/js/components/DateRangeFilter.vue
+++ resources/js/components/DateRangeFilter.vue
@@ -2,21 +2,27 @@
   <div>
     <h3 class="text-sm uppercase tracking-wide text-80 bg-30 p-3">{{ filter.name }}</h3>
 
-    <div class="p-2">
+    <div class="p-2 flex items-center">
       <input
-        class="w-full form-control form-input form-input-bordered"
-        :disabled="disabled"
-        :class="{'!cursor-not-allowed': disabled}"
-        :value="value"
-        ref="datePicker"
-        type="text"
-        :placeholder="placeholder"
+          class="w-full form-control form-input form-input-bordered"
+          :disabled="disabled"
+          :class="{'!cursor-not-allowed': disabled}"
+          :value="value"
+          ref="datePicker"
+          type="text"
+          :placeholder="placeholder"
       />
+      <button v-if="value && value.length" @click="clearFilter()" class="ml-2 text-danger inline-flex items-center focus:outline-none">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+      </button>
     </div>
   </div>
 </template>
 <script>
 import flatpickr from 'flatpickr'
+import {Turkish} from 'flatpickr/dist/l10n/tr.js';
 import '../../airbnb-modified.css'
 
 export default {
@@ -55,7 +61,7 @@ export default {
     },
     options() {
       return this.$store.getters[`${this.resourceName}/getOptionsForFilter`](
-        this.filterKey
+          this.filterKey
       )
     },
     disabled() {
@@ -65,10 +71,10 @@ export default {
       return this.filter.separator || '-'
     },
     modeType() {
-        return (this.filter.mode === 'range') ? 'range' : 'single'
+      return (this.filter.mode === 'range') ? 'range' : 'single'
     },
     dateFormat() {
-        return this.filter.dateFormat || (this.filter.enableTime ? 'Y-m-d H:i' : 'Y-m-d')
+      return this.filter.dateFormat || (this.filter.enableTime ? 'Y-m-d H:i' : 'Y-m-d')
     },
     twelveHourTime() {
       return this.filter.twelveHourTime
@@ -102,30 +108,43 @@ export default {
         onReady() {
           self.$refs.datePicker.parentNode.classList.add('date-filter')
         },
-        locale: {
-          rangeSeparator: ` ${this.separator} `,
-          firstDayOfWeek: this.firstDayOfWeek
-        }
+        locale: Turkish,
+        // {
+        //   rangeSeparator: ` ${this.separator} `,
+        //   firstDayOfWeek: this.firstDayOfWeek,
+        // }
       })
     })
   },
 
   methods: {
     handleChange(value) {
-        setTimeout(() => {
-            value = value.map(value => {
-                return flatpickr.formatDate(value, this.dateFormat)
-            })
+      setTimeout(() => {
+        value = value.map(value => {
+          return flatpickr.formatDate(value, this.dateFormat)
+        })
 
-            if (Object.keys(value).length !== 0) {
-                this.$store.commit(`${this.resourceName}/updateFilterState`, {
-                    filterClass: this.filterKey,
-                    value,
-                });
-                this.$emit('change')
-            }
-        }, 100)
+        if (Object.keys(value).length !== 0) {
+          this.$store.commit(`${this.resourceName}/updateFilterState`, {
+            filterClass: this.filterKey,
+            value,
+          });
+          this.$emit('change')
+        }
+      }, 100)
     },
+    async clearFilter() {
+      this.flatpickr.clear();
+
+      setTimeout(() => {
+        this.$store.commit(`${this.resourceName}/updateFilterState`, {
+          filterClass: this.filterKey,
+          value: [],
+        });
+
+        this.$emit('change')
+      }, 100)
+    }
   }
 }
 </script>
